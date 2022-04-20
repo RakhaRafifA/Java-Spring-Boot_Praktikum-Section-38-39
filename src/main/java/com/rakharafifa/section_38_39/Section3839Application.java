19d9406c255cf83b9a0735b18e5db2a27ef5fdc0
RakhaRafifArifin <rakhara104@gmail.com>
2022-04-20 11:35:31 +0700

    praktikum section 38 dan 39

---
+++ src/main/java/com/rakharafifa/section_38_39/Section3839Application.java
@@ -0,0 +1,13 @@
+package com.rakharafifa.section_38_39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3839Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3839Application.class, args);
+	}
+
+}
